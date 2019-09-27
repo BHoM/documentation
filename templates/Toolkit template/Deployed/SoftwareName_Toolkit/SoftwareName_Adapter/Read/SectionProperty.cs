d8a886e38f17901b47be8da5afa5cce9c8ef5e4b
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-09-27 13:24:27 +0100

    update

---
+++ templates/Toolkit template/Deployed/SoftwareName_Toolkit/SoftwareName_Adapter/Read/SectionProperty.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Structure.SectionProperties;
+using BH.oM.Common.Materials;
+
+namespace BH.Adapter.SoftwareName
+{
+    public partial class $safeprojectname$
+    {
+
+        /***************************************************/
+        /**** Private methods                           ****/
+        /***************************************************/
+
+        //The List<string> in the methods below can be changed to a list of any type of identification more suitable for the toolkit
+        //If no ids are provided, the convention is to return all elements of the type
+
+        private List<ISectionProperty> ReadSectionProperties(List<string> ids = null)
+        {
+            //Implement code for reading section properties
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+
+    }
+}
