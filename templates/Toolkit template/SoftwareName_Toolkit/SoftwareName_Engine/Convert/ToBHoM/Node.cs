c6f479045dd220db71489c200588a4186562c914
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-07 16:43:49 +0100

    Update Node.cs

--- templates/Toolkit template/SoftwareName_Toolkit/SoftwareName_Engine/Convert/ToBHoM/Node.cs
+++ templates/Toolkit template/SoftwareName_Toolkit/SoftwareName_Engine/Convert/ToBHoM/Node.cs
@@ -11,6 +11,7 @@
  * the Free Software Foundation, either version 3.0 of the License, or          
  * (at your option) any later version.                                          
  *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
  * but WITHOUT ANY WARRANTY; without even the implied warranty of               
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
  * GNU Lesser General Public License for more details.                          
